9563066592cd86d5f1f27b899a090cbfcdb33347
bmgcosta <bmgcosta@outlook.com>
2022-05-13 15:31:49 +0100

    Add message onWritting

--- view/components/dashboard/dashboard.js
+++ view/components/dashboard/dashboard.js
@@ -7,8 +7,10 @@ import { addNewMessage } from "../../events/form/addNewMessage.js";
 const dashboard = function(div,msg){
     const divDash = addElem('div', ['flex','flex-col','w-full', 'min-h-[400px]', 'h-screen', 'justify-between', 'p-10'], 'dashboard');    
     const msgDiv = addElem('div', ['w-full', 'overflow-auto', 'mb-5', 'px-2'], 'messages');
+    const msgForm = addElem('div', ['w-full', 'mb-5', 'px-2'], 'formAction');
     msg.forEach(msgOne => divMsg(msgDiv,msgOne));
-    divDash.append(msgDiv, formNewMessage());
+    msgForm.append(formNewMessage());
+    divDash.append(msgDiv, msgForm);
     add(document, 'addNewMessage', addNewMessage);
     div.append(divDash);
 }
--- view/components/form/formNewMessage.js
+++ view/components/form/formNewMessage.js
@@ -2,11 +2,13 @@ import { addElem } from "../../../helpers/dom/elem.js";
 import { input } from "../../../helpers/form/input.js";
 import {submitFormMessage} from "../../events/form/submitFormMessage.js";
 import { add } from "../../../helpers/dom/events.js";
+import { onWriting } from "../../events/form/onWriting.js";
 
 const formNewMessage = function(){
     const form = addElem('form', ['flex', 'gap-3']);
     const inputMessage = input('message', 'text', 'Your message', ['bg-slate-200', 'p-3', 'grow']);
-    const inputSubmit = input('submitMessage', 'submit', 'Send', ['h-full', 'bg-slate-900', 'px-3', 'text-white', 'font-bold', 'cursor-pointer']);
+    add(inputMessage, 'keyup', onWriting);
+    const inputSubmit = input('submitMessage', 'submit', 'Send', ['bg-slate-900', 'px-3', 'text-white', 'font-bold', 'cursor-pointer']);
     add(form, 'submit', submitFormMessage);
     form.append(inputMessage, inputSubmit);
     return form;
