860f07d151042427d2472b3ed6d1e85bd90bcbd3
bmgcosta <bmgcosta@outlook.com>
2022-05-13 14:32:41 +0100

    Change order by ASC message

--- infra/Database/Localstorage/LocalStorage.js
+++ infra/Database/Localstorage/LocalStorage.js
@@ -1,10 +1,10 @@
 import DatabaseRules from "../DatabaseRules/DatabaseRules.js";
 
 class LocalStorage extends DatabaseRules{
-    all = () => Object.values(window.localStorage).map(msg => JSON.parse(msg));
+    all = () => Object.values(window.localStorage).map(msg => JSON.parse(msg)).sort((a,b) => a.dateTime > b.dateTime);
     get = (id) => JSON.parse(window.localStorage.getItem(id));
-    add = (id, contact) => window.localStorage.setItem(id, JSON.stringify(contact));
-    upd = (id,contact) => window.localStorage.setItem(id, JSON.stringify(contact));
+    add = values => window.localStorage.setItem(values.id, JSON.stringify(values));
+    upd = values => window.localStorage.setItem(values.id, JSON.stringify(values));
     del = (id) => window.localStorage.removeItem(id);
     delAll = () => window.localStorage.clear();
 }
