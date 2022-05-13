b89ff73aa2979f97ef8dabb18cce9360fa6d2d9b
bmgcosta <bmgcosta@outlook.com>
2022-05-13 10:00:03 +0100

    Change name method getAll to all

--- infra/Database/Localstorage/LocalStorage.js
+++ infra/Database/Localstorage/LocalStorage.js
@@ -1,7 +1,7 @@
 import DatabaseRules from "../DatabaseRules/DatabaseRules.js";
 
 class LocalStorage extends DatabaseRules{
-    getAll = () => Object.entries(window.localStorage).sort((a,b) => a.at(0) > b.at(0));
+    all = () => Object.entries(window.localStorage).sort((a,b) => a.at(0) > b.at(0));
     get = (id) => JSON.parse(window.localStorage.getItem(id));
     add = (id, contact) => window.localStorage.setItem(id, JSON.stringify(contact));
     upd = (id,contact) => window.localStorage.setItem(id, JSON.stringify(contact));
